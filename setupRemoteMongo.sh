d4e5f4afe5d90904e40341859edf135297b6eb0b
happyhemantsingh58 <happyhemantsingh4403@gmail.com>
2022-11-24 21:09:05 +0530

    latest change

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,3 +1,3 @@
 # Setup file template to upload data to MongoDB Atlas
-mongoimport --uri "mongodb+srv://happyhemantsingh:happyhemantsingh@qkart-node.n0o2p7d.mongodb.net/?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri "mongodb+srv://happyhemantsingh:happyhemantsingh@qkart-node.n0o2p7d.mongodb.net/?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
+mongoimport --uri "mongodb+srv://admin:Admin555@qkart-node.n0o2p7d.mongodb.net/?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb+srv://admin:Admin555@qkart-node.n0o2p7d.mongodb.net/?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
